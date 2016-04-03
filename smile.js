7b7cead214e32c7647a94bcb65586a60d4f63e5b
Ryan Rouleau <ryan.c.rouleau@live.com>
2016-04-03 04:14:26 -0600

    this should work

--- smile.js
+++ smile.js
@@ -14,51 +14,55 @@ if (Meteor.isClient) {
 Router.route('/dashboard', function () {
     this.render('dashboard');
     if (Meteor.isClient) {
-        if (firstRun == true) {
-            google.charts.load('current', {'packages':['bar']});
-            google.charts.setOnLoadCallback(drawChart);
-            firstRun = false;
-        }
-        console.log("histogram should load");
-          // ***** Histogram *****
-      function drawChart() {
-          timeArray = [];
-          $().ready(function() {
-              SmileList.find().forEach(function(obj){
-                date = new Date(obj.time);
-                hours = date.getHours()+7;
-                found = false;
-                for (var i=0; i<timeArray.length; i++) {
-                    if (timeArray[i][0][0] == hours) {
-                        timeArray[i][1] += 1;
-                        found = true;
+        if (Session.get('data_loaded')) {
+            if (firstRun == true) {
+                google.charts.load('current', {'packages':['bar']});
+                google.charts.setOnLoadCallback(drawChart);
+                firstRun = false;
+            }
+            console.log("histogram should load");
+              // ***** Histogram *****
+          function drawChart() {
+              timeArray = [];
+              $().ready(function() {
+                  SmileList.find().forEach(function(obj){
+                    date = new Date(obj.time);
+                    hours = date.getHours()+7;
+                    found = false;
+                    for (var i=0; i<timeArray.length; i++) {
+                        if (timeArray[i][0][0] == hours) {
+                            timeArray[i][1] += 1;
+                            found = true;
+                        }
                     }
-                }
-                if (!found) {
-                    timeArray.push([[hours,0,0],1]);
-                }
-                console.log(obj.description);
+                    if (!found) {
+                        timeArray.push([[hours,0,0],1]);
+                    }
+                    console.log(obj.description);
+                  });
+              })
+              data = new google.visualization.DataTable();
+              data.addColumn('timeofday', 'Time of Day');
+              data.addColumn('number', 'Number of smiles');
+              data.addRows(timeArray);
+              options = google.charts.Bar.convertOptions({
+                title: 'Number of smiles at each hour',
+                height: 450,
+                legend: { position: 'none' },
+                colors: ['#80DEEA']
               });
-          })
-          data = new google.visualization.DataTable();
-          data.addColumn('timeofday', 'Time of Day');
-          data.addColumn('number', 'Number of smiles');
-          data.addRows(timeArray);
-          options = google.charts.Bar.convertOptions({
-            title: 'Number of smiles at each hour',
-            height: 450,
-            legend: { position: 'none' },
-            colors: ['#80DEEA']
-          });
-          chart = new google.charts.Bar(document.getElementById('chart_div'));
-          chart.draw(data, options);
+              chart = new google.charts.Bar(document.getElementById('chart_div'));
+              chart.draw(data, options);
+            }
         }
-    }
-    if (numRuns % 2 != 0) {
-        window.location.replace('/dashboard');
-    }
-    numRuns++;
-    console.log(numRuns);
+        if (numRuns % 2 == 0) {
+            //console.log("numRuns:"+numRuns);
+            //location.reload();
+            //window.location.replace('/dashboard');
+        }
+        numRuns++;
+        console.log(numRuns);
+        }
 });
 
 Router.route('/smile', function () {
@@ -96,6 +100,14 @@ Router.route('/edit', function() {
 
 // ***** Start Meteor Location Conditionals *****
 if (Meteor.isClient) {
+    Meteor.startup(function() {
+       Session.set('data_loaded', false);
+    });
+
+    Meteor.subscribe('default_db_data', function(){
+       //Set the reactive session as true to indicate that the data have been loaded
+       Session.set('data_loaded', true);
+    });
     Template.histogram.onCreated(function(){
         //location.reload();
     });
@@ -158,7 +170,7 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.publish('default_db_data', function(){
+        return SmileList.find({});
+    });
 }
